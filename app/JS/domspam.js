6dfcc3c11446012f320490359bdf3270e69b9976
ryans439 <ryans439@nycstudents.net>
2024-11-25 20:52:05 -0500

    d

---
+++ app/JS/domspam.js
@@ -0,0 +1,11 @@
+export const DOMSelectors = {
+    itemcontainer: document.querySelector(".itemcontainer"),
+    h1: document.querySelector(".heading"),
+    h2: document.querySelector("#heading2"),
+    itemSearchForm: document.querySelector(".form"),
+    itemSearchValue: document.querySelector(".value"),
+    resetbutton: document.querySelector(".resetbutton"),
+    dropdown: document.querySelector(".dropdown"),
+    card: document.querySelector(".card"),
+};
+  
